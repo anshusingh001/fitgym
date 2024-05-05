aee7b0a14b169aeb77f680f66de0671de2d43f0b
Anshu Kumar Singh <anshusinghak001@gmail.com>
2024-05-05 13:53:32 +0530

    check

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
 
     <footer className="footer">
       <Box mt="80px" bgcolor="#FFF3F4" height="150px">
-        <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made with ❤️</Typography>
+        <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made withhh ❤️</Typography>
       </Box>
       <div class="footerContainer">
         <p class="copyright">copyright© Anshu Singh</p>
