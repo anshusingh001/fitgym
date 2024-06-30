6cabbf46da212ef92f3c1face71ffe8e3fcfd93b
Anshu Kumar Singh <anshusinghak001@gmail.com>
2024-06-30 17:38:49 +0530

    some bug fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,10 @@ const Footer = () => {
 
     <footer className="footer">
       <Box mt="80px" bgcolor="#FFF3F4" height="150px">
-        <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made withhh ❤️</Typography>
+        <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made with ❤️</Typography>
       </Box>
-      <div class="footerContainer">
-        <p class="copyright">copyright© Anshu Singh</p>
+      <div className="footerContainer">
+        <p className="copyright">copyright© Anshu Singh</p>
       </div>
     </footer>
 
